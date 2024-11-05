e8e90f9b6b4acc84532a55804c6d7108b0010a1a
OmarEiissa <eissa0383@gmail.com>
2024-11-05 03:36:56 +0200

    add links in footer, smooth show filter in sm media

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import { assets } from "../assets/assets";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -15,10 +16,10 @@ const Footer = () => {
         <div>
           <p className="text-xl font-medium mb-5">COMPANY</p>
           <ul className="flex flex-col gap-1 text-gray-600">
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Delivery</li>
-            <li>Privacy Policy</li>
+            <Link to="/">Home</Link>
+            <Link to="/about">About Us</Link>
+            <Link to={"/orders"}>Delivery</Link>
+            <Link to={"/contact"}>Contact Us</Link>
           </ul>
         </div>
 
--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -107,85 +107,85 @@ const Collection = () => {
           />
         </p>
 
-        {/* Category Filter */}
-        <div
-          className={`border border-gray-300 pl-5 py-3 mt-6 ${
-            showFilter ? "" : "hidden"
-          } sm:block`}
-        >
-          <p className="mb-3 text-sm font-medium">CATEGORIES</p>
+        <div className="relative">
+          <div
+            className={`absolute bg-white w-full top-0 left-0 ${
+              showFilter ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"
+            } sm:max-h-none sm:opacity-100 sm:block transition-all duration-300 ease-in-out`}
+          >
+            {/* Category Filter */}
+            <div className={`border border-gray-300 pl-5 py-3 mt-6 sm:block`}>
+              <p className="mb-3 text-sm font-medium">CATEGORIES</p>
 
-          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
-            <p className="flex gap-2">
-              <input
-                type="checkbox"
-                className="w-3"
-                value={"Men"}
-                onClick={toggleCategory}
-              />{" "}
-              Man
-            </p>
+              <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+                <p className="flex gap-2">
+                  <input
+                    type="checkbox"
+                    className="w-3"
+                    value={"Men"}
+                    onClick={toggleCategory}
+                  />{" "}
+                  Man
+                </p>
 
-            <p className="flex gap-2">
-              <input
-                type="checkbox"
-                className="w-3"
-                value={"Women"}
-                onClick={toggleCategory}
-              />{" "}
-              Women
-            </p>
+                <p className="flex gap-2">
+                  <input
+                    type="checkbox"
+                    className="w-3"
+                    value={"Women"}
+                    onClick={toggleCategory}
+                  />{" "}
+                  Women
+                </p>
 
-            <p className="flex gap-2">
-              <input
-                type="checkbox"
-                className="w-3"
-                value={"Kids"}
-                onClick={toggleCategory}
-              />{" "}
-              Kids
-            </p>
-          </div>
-        </div>
+                <p className="flex gap-2">
+                  <input
+                    type="checkbox"
+                    className="w-3"
+                    value={"Kids"}
+                    onClick={toggleCategory}
+                  />{" "}
+                  Kids
+                </p>
+              </div>
+            </div>
 
-        {/* SubCategory Filter */}
-        <div
-          className={`border border-gray-300 pl-5 py-3 my-5 ${
-            showFilter ? "" : "hidden"
-          } sm:block`}
-        >
-          <p className="mb-3 text-sm font-medium">TYPE</p>
+            {/* SubCategory Filter */}
+            <div className={`border border-gray-300 pl-5 py-3 my-5 sm:block`}>
+              <p className="mb-3 text-sm font-medium">TYPE</p>
 
-          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
-            <p className="flex gap-2">
-              <input
-                type="checkbox"
-                className="w-3"
-                value={"Topwear"}
-                onClick={toggleSubCategory}
-              />
-              Topwear
-            </p>
+              <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+                <p className="flex gap-2">
+                  <input
+                    type="checkbox"
+                    className="w-3"
+                    value={"Topwear"}
+                    onClick={toggleSubCategory}
+                  />
+                  Topwear
+                </p>
 
-            <p className="flex gap-2">
-              <input
-                type="checkbox"
-                className="w-3"
-                value={"Bottomwear"}
-                onClick={toggleSubCategory}
-              />
-              Bottomwear
-            </p>
+                <p className="flex gap-2">
+                  <input
+                    type="checkbox"
+                    className="w-3"
+                    value={"Bottomwear"}
+                    onClick={toggleSubCategory}
+                  />
+                  Bottomwear
+                </p>
 
-            <p className="flex gap-2">
-              <input
-                type="checkbox"
-                className="w-3"
-                value={"Winterwear"}
-                onClick={toggleSubCategory}
-              />
-              Winterwear
-            </p>
+                <p className="flex gap-2">
+                  <input
+                    type="checkbox"
+                    className="w-3"
+                    value={"Winterwear"}
+                    onClick={toggleSubCategory}
+                  />
+                  Winterwear
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
